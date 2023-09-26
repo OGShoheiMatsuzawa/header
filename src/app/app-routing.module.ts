1d8bc4f8970055f0ea40f3c108e5c5688a8b009c
OGShoheiMatsuzawa <143977209+OGShoheiMatsuzawa@users.noreply.github.com>
2023-09-26 17:23:47 +0900

    Add header component base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HeaderComponent } from './header/header.component';
+
+const routes: Routes = [
+ { path: 'header', component: HeaderComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
