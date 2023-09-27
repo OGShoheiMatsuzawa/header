662004b932555d864de0a9732267e0b65223de4f
OGShoheiMatsuzawa <143977209+OGShoheiMatsuzawa@users.noreply.github.com>
2023-09-27 14:10:47 +0900

    Add posts from API

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 import { Header } from '../header';
 import { HEADER } from '../mock';
@@ -8,8 +9,27 @@ import { HEADER } from '../mock';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
 
-  header: Header = HEADER
+export class HeaderComponent implements OnInit {
+  
+  public header: Header = HEADER
+
+  private postsUrl: string = 'https://dev-sandbox-mat01-be.sakuramobile.jp/api/v1/posts'
+  public posts: any
+
+  constructor(
+    private http: HttpClient
+  ){}
+
+  ngOnInit() {
+    this.getPostData();
+  }
+
+  getPostData() {
+    this.http.get(this.postsUrl)
+    .subscribe((result: any) => {
+      this.posts = result.data; // keyがdata, valueが投稿の配列。左記はRails APIのjson出力で設定
+    });
+  }
 
 }
